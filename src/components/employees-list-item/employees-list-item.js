3d783cb558ca2d025048c7e521917986f303ab44
glory1357 <efim133@mail.ru>
2021-12-10 21:13:05 +0300

    practicalReact form toggleIncrease toggleProp

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -2,7 +2,7 @@ import './employees-list-item.css';
 
 const EmployeesListItem = (props) => {
 
-    const {name, salary, onDelete, onToggleIncrease, onToggleRise, increase, rise} = props;
+    const {name, salary, onDelete, onToggleProp, increase, rise} = props;
 
     let className = 'list-group-item d-flex justify-content-between';
     if (increase) {
@@ -15,12 +15,13 @@ const EmployeesListItem = (props) => {
     
     return (
         <li className={className}>
-            <span onClick={onToggleRise} className="list-group-item-label">{name}</span>
+            <span onClick={onToggleProp} data-toggle="rise" className="list-group-item-label">{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
                     className="btn-cookie btn-sm "
-                    onClick={onToggleIncrease}>
+                    onClick={onToggleProp}
+                    data-toggle="increase">
                     <i className="fas fa-cookie"></i>
                 </button>
 
